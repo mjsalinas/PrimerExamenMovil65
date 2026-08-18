3aeb6027608baff6aa3c57d17342b02cc8579f53
douglasdjorkaeff <douglas777luz@gmail.com>
2026-08-17 21:00:26 -0600

    En AppNavigator se agrego un scrrenOptions para headershown para el falso encabezado

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GameScreen from '../screens/GameScreen';
 import ResultScreen from '../screens/ResultScreen';
@@ -6,7 +7,7 @@ const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="GameScreen" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="GameScreen" component={GameScreen} />
       <Stack.Screen name="ResultScreen" component={ResultScreen} />
     </Stack.Navigator>
