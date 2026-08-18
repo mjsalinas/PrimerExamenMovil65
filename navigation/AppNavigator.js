daad027e55d206823209fb678eb7e3bbf32f333c
Stefany <stefany_madrid@unitec.edu>
2026-08-17 20:54:17 -0600

    -Se hizo correccion en la libreria  @react-navation/native-stack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,4 +1,4 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native';
 import GameScreen from '../screens/GameScreen';
 import ResultScreen from '../screens/ResultScreen';
 
