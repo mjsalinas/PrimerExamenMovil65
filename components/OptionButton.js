98a39408543df2b91e47012322cc49b4cebf305e
walterdiaz1993 <waltermisael1108@gmail.com>
2026-08-17 21:04:50 -0600

    OptionButton - se agrego color al presionar el boton y soltarlo

--- components/OptionButton.js
+++ components/OptionButton.js
@@ -1,11 +1,13 @@
-import { Pressable, Text, StyleSheet, View } from 'react-native';
+import { useState } from "react";
+import { Pressable, Text, StyleSheet, View } from "react-native";
 
 export default function OptionButton({ label, onPress, disabled }) {
+  const [colorOnPress, setColorOnPress] = useState("#fff");
   const styles = StyleSheet.create({
     button: {
-      backgroundColor: disabled ? '#B0BEC5' : '#FFFFFF', // BUG INTENCIONAL
+      backgroundColor: disabled ? "#B0BEC5" : colorOnPress, // BUG INTENCIONAL
       borderWidth: 2,
-      borderColor: '#4A90D9',
+      borderColor: "#4A90D9",
       borderRadius: 10,
       paddingVertical: 14,
       paddingHorizontal: 16,
@@ -14,17 +16,23 @@ export default function OptionButton({ label, onPress, disabled }) {
     },
     label: {
       fontSize: 16,
-      color: disabled ? '#546E7A' : '#212121',
-      textAlign: 'center',
-      fontWeight: '600',
+      color: disabled ? "#546E7A" : "#212121",
+      textAlign: "center",
+      fontWeight: "600",
     },
   });
 
   return (
-    <Pressable onPress={onPress} style={styles.button} disabled={disabled}> 
-    <View>
-      <Text style={styles.label}>{label}</Text>
-    </View>
+    <Pressable
+      onPress={onPress}
+      style={styles.button}
+      disabled={disabled}
+      onPressIn={() => setColorOnPress("#E3F2FD")}
+      onPressOut={() => setColorOnPress("#fff")}
+    >
+      <View>
+        <Text style={styles.label}>{label}</Text>
+      </View>
     </Pressable>
   );
 }
