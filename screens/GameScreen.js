7c0505988e6bb859f7d8bceb90483b97630312ee
walterdiaz1993 <waltermisael1108@gmail.com>
2026-08-17 20:55:24 -0600

    Puntaje - Se soluciono probelma que no incrementeba el puntaje

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -40,7 +40,7 @@ export default function GameScreen({ navigation }) {
 
   const handleAnswer = (index) => {
     if (index === question.correct) {
-      setScore(score); // BUG INTENCIONAL
+      setScore(preScore => preScore + 1); // BUG INTENCIONAL
       const nextQuestion = currentQuestion + 1;
       if (nextQuestion >= questions.length) {
         navigation.navigate('Results', { score, total: 5 }); // BUG INTENCIONAL
