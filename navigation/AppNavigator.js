22ea53bd52372b9bd8229d166626800ac35545e5
byrontorres26 <byrontorres@unitec.edu>
2026-08-17 20:08:18 -0600

    App Navigator Creacion de initial Route Name

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,13 +2,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GameScreen from '../screens/GameScreen';
 import ResultScreen from '../screens/ResultScreen';
 
+
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator> {/* BUG INTENCIONAL */}
-      <Stack.Screen name="GameScreen" component={GameScreen} />
-      <Stack.Screen name="ResultScreen" component={ResultScreen} />
+    <Stack.Navigator initialRouteName='GameScreen' screenOptions = {{headerShown: false}}> 
+      <Stack.Screen name="GameScreen" component={GameScreen}/>
+      <Stack.Screen name="ResultScreen" component={ResultScreen}/>
     </Stack.Navigator>
   );
 }
